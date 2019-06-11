594f888aea87ac7e020bda7c3369b8ebb6716e54
Atasha Potter <Ms.APotter@gmail.com>
2019-06-10 20:33:22 -0400

    sample accommodation json works

--- db/seed.js
+++ db/seed.js
@@ -7,20 +7,22 @@ const { Accomodation, Reviews, User } = require('./models.js')
 
 const accomodationData = require('./data/accomodation.json')
 const userData = require('./data/users.json')
-const reviewData = require('./data/reviews')
+const reviewData = require('./data/reviews.json')
 
-Accomodation.deleteMany({}).then(() => {
+Accomodation.find({}).deleteMany({}).then(() => {
     console.log('deleted all accomodations')
     User.deleteMany({}).then(() => {
       console.log('deleted all users')
       Reviews.deleteMany({}).then(() => {
         console.log('deleted all reviews')
 
-            Accomodation.create({accomodationData})
-            .then((accomodationData => {
-                console.log(accomodationData);
-              }))
-              .catch(error => console.log('Uh oh!', error))
+            Accomodation.create(accomodationData)
+            .then((data) => {
+                console.log(data);
+              })
+            .catch(error => console.log('Uh oh!', error))
+
+            
  
       })
     })
