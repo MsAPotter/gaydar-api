2b1fc3e8f601db29172b9cc4c9e73fdbda341f67
Atasha Potter <Ms.APotter@gmail.com>
2019-06-12 14:06:39 -0400

    fixed review route controller

--- controllers/reviewControl.js
+++ controllers/reviewControl.js
@@ -2,25 +2,34 @@ const { Reviews, Accomodation, User} = require('../db/models');
 
 module.exports = {
     index: (req, res) => {
-        Reviews.find({}).then(reviews => {
+        Reviews.find({})
+        .then(reviews => {
             res.json(reviews)
           })
     },
     findById: (req, res) => {
-        Reviews.findOne({_id: req.params.reviewId}).then((review) => 
-        { res.json(review); })
+        Reviews.findOne({_id: req.params.reviewId})
+        .then((review) => { 
+            res.json(review)
+        })
     },
     create: (req, res) => {
-        Reviews.create({}).then((created) => {
+        Reviews.create(req.body)
+        .then((created) => {
             res.json(created)
         })
     },
     update: (req, res) => {
-        Reviews.updateOne({_id: req.params.reviewId}).then((review) => 
-        { res.json(review); })
+        Reviews.updateOne({ _id: req.params.reviewId}, req.body)
+        .then((review) => {
+            res.json(review) 
+        })
     },
     delete: (req, res) => {
-        Reviews.findOne({_id: req.params.reviewId}).then((review) => 
-        { Reviews.deleteOne ({_id: req.params.reviewId})})
+        Reviews.findOneAndDelete({_id: req.params.reviewId})
+        .then((deleted) => { 
+            res.json(deleted)
+    })
     }
+
 }
